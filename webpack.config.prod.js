5b46a0ab4b18749ae26cc0f5c2c3149c2f4fa382
Nat Homer <nathaniel.ott.homer@gmail.com>
2016-06-14 17:48:18 -0400

    adds production version of react

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/should_reads.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  "scripts": {
+   "heroku-postbuild": "webpack --config webpack.config.prod.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};

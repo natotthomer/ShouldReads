2d1acc9eb495b6b4cec2cb6cf5c7abd19430301e
Nat Homer <nathaniel.ott.homer@gmail.com>
2016-06-14 18:00:24 -0400

    fixes minified react

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,6 +7,18 @@ module.exports = {
     path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   module: {
     loaders: [
       {

259ee122019b638dd519e24b24e413851f86c8c7
Nat Homer <nathaniel.ott.homer@gmail.com>
2016-06-14 17:57:59 -0400

    fixes package.json

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,10 +4,8 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/should_reads.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js",
-    devtoolModuleFilenameTemplate: '[resourcePath]',
-    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+    path: "./app/assets/javascripts",
+    filename: "bundle.js"
   },
   module: {
     loaders: [
